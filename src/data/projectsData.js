463413229de316164326cacbc5d16f47532fb7ff
Siddharth <officialsiddharthbisht@gmail.com>
2022-08-04 14:24:28 +0530

    ADD: Nykaa clone website link , deployment link etc in the portfolio

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,6 +11,23 @@
 export const projectsData = [
   {
     id: 1,
+    projectName: "Nykaa Clone",
+    projectDesc: [
+      `Nykaa is an E-commerce company have domain around cosmetic's`,
+      [
+        "Front-End Using React",
+        "Back-End using Node Express",
+        "Database using MongoDB",
+      ],
+    ],
+    tags: ["Mongo DB", "Express JS", "React-JS","Node JS","Redux"],
+    code: "https://github.com/OfficialSiddharthBisht/nykaa-clone",
+    demo: "https://nykaa-web-app.netlify.app/",
+    image:
+      "https://www.bing.com/images/search?q=nykaa%20website&qs=MT&form=QBIR&sp=1&pq=nykaa%20we&sc=10-8&cvid=AAC179347D534944829CF6D7ED0209CF&ghsh=0&ghacc=0&first=1&tsc=ImageHoverTitle",
+  },
+  {
+    id: 2,
     projectName: "Lumen 5 Clone",
     projectDesc: [
       `Lumen5 is a video creation platform powered by A.I. that enables anyone without training or experience to easily create engaging video content within minutes.`,
@@ -28,7 +45,7 @@ export const projectsData = [
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F50wheel.com%2Fwp-content%2Fuploads%2F2018%2F05%2Flumen5-3.png&f=1&nofb=1",
   },
   {
-    id: 2,
+    id: 3,
     projectName: "Discovery+ Clone",
     projectDesc: [
       "Discovery+ is a streaming service. Discovery.The service focuses on factual programming drawn from the libraries of Discovery's main channel brands, as well as original series and other acquired content.",
@@ -41,7 +58,7 @@ export const projectsData = [
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.dBgoyEfYljjeyDFAxa-twwHaEU%26pid%3DApi&f=1",
   },
   {
-    id: 3,
+    id: 4,
     projectName: "Expedia Travel Clone",
     projectDesc: [
       "Expedia Group, Inc. is an American online travel shopping company for consumer and small business travel. Its websites, which are primarily travel fare aggregators and travel metasearch engines",
@@ -58,7 +75,7 @@ export const projectsData = [
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Y2gvAVgkMcupwaefIabawQHaE6%26pid%3DApi&f=1",
   },
   {
-    id: 4,
+    id: 5,
     projectName: "Tesla Clone",
     projectDesc: [
       "Tesla is accelerating the world's transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses.",
@@ -74,7 +91,7 @@ export const projectsData = [
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.f9OvjIR-m7YLjEqv-wJL-QHaJq%26pid%3DApi&f=1",
   },
   {
-    id: 4,
+    id: 6,
     projectName: "Weather App",
     projectDesc: [
       "A weather app that shows the weather of any city in the world.",
